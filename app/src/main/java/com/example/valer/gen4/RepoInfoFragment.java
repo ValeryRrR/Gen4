beeea7edc4725e85be88d1b945932487ebe591cb
Valery Dobin <valerydobin@gmail.com>
2019-02-09 12:39:00 +0300

    Added RepoInfoFragment

---
+++ app/src/main/java/com/example/valer/gen4/RepoInfoFragment.java
@@ -0,0 +1,36 @@
+package com.example.valer.gen4;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.valer.gen4.R;
+
+public class RepoInfoFragment extends Fragment {
+
+    TextView repoName;
+    TextView description;
+
+    public RepoInfoFragment(){
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_repo_info, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        repoName = view.findViewById(R.id.repoName);
+        description = view.findViewById(R.id.description);
+
+    }
+}
